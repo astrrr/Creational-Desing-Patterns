9b314278939bc77a3bf77e6520537341bc28aac5
Sekson Thongon <invisibile.sek@gmail.com>
2021-10-15 22:06:25 +0700

    update usge Main.java

--- SoftArchDesign/src/main/java/creational/Main.java
+++ SoftArchDesign/src/main/java/creational/Main.java
@@ -5,25 +5,33 @@ import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParserConfigurationException, IOException {
 
         // Current usage
-        BookMetadataFormatter formatter = null;
-        try {
-            formatter = BookMetadataFormatterFactory.getBookMetadataFormatter(BookMetadataFormatterFactory.Format.CSV);
-            formatter.append(TestData.dragonBook);
-            formatter.append(TestData.dinosaurBook);
-            System.out.print(formatter.getMetadataString());
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParserConfigurationException e) {
-            e.printStackTrace();
-        }
+//        BookMetadataFormatter formatter = null;
+//        try {
+//            formatter = BookMetadataFormatterFactory.getBookMetadataFormatter(BookMetadataFormatterFactory.Format.CSV);
+//            formatter.append(TestData.dragonBook);
+//            formatter.append(TestData.dinosaurBook);
+//            System.out.print(formatter.getMetadataString());
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } catch (ParserConfigurationException e) {
+//            e.printStackTrace();
+//        }
 
         // Expected usage
-//        BookMetadataExporter exporter = new XMLBookMetadataExporter();
-//        exporter.add(TestData.sailboatBook);
-//        exporter.add(TestData.GoFBook);
-//        exporter.export(System.out);
+        BookMetadataExporter exporter = new XMLBookMetadataExporter();
+        exporter.add(TestData.sailboatBook);
+        exporter.add(TestData.GoFBook);
+        exporter.export(System.out);
+
+        BookMetadataExporter exporter1 = new CSVBookMetadataExporter();
+        exporter1.add(TestData.dragonBook);
+        exporter1.export(System.out);
+
+        BookMetadataExporter exporter2 = new JSONBookMetadataExporter();
+        exporter2.add(TestData.dinosaurBook);
+        exporter2.export(System.out);
     }
 }
